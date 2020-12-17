3723784a76869007124001a6f6e3893d0753d6e9
yeetologist <makanmakan9c@gmail.com>
2020-12-17 21:37:13 +0700

    colour revision & changing main to master

--- dwm/config.h
+++ dwm/config.h
@@ -15,12 +15,13 @@ static int swallowfloating    = 0;        /* 1 means swallow floating windows by
 static int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 0;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
-static char *fonts[]          = { "monospace:size=10", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  }; static char normbgcolor[]           = "#222222";
-static char normbordercolor[]       = "#222";
-static char normfgcolor[]           = "#444";
-static char selfgcolor[]            = "#bbb";
-static char selbordercolor[]        = "#eee";
-static char selbgcolor[]            = "#f59542";
+static char *fonts[]          = { "monospace:size=10", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
+static char normbgcolor[]           = "#222";
+static char normbordercolor[]       = "#444";
+static char normfgcolor[]           = "#bbb";
+static char selfgcolor[]            = "#eee";
+static char selbordercolor[]        = "#fff";
+static char selbgcolor[]            = "#000";
 static char *colors[][3] = {
        /*               fg           bg           border   */
        [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
