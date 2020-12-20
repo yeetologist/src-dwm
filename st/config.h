650b74d55ed8146a954f5d2781e19f0c6a9188f0
yeetologist <makanmakan9c@gmail.com>
2020-12-20 19:36:48 +0700

    adding foreign font on st

--- st/config.h
+++ st/config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "MesloLGS NF:pixelsize=14:antialias=true:autohint=true";
-static char *font2[] = { "JetBrains Mono Medium:pixelsize=11:antialias=true:autohint=true" };
+static char *font2[] = { "Source Han Sans:pixelsize=11:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
