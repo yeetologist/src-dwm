1c08d23f1522b17bbd57ee827dad9d5795e9d426
yeetologist <makanmakan9c@gmail.com>
2020-12-31 13:40:03 +0700

    0 pixels border

--- dwm/config.h
+++ dwm/config.h
@@ -5,7 +5,7 @@
 #define TERMCLASS "St"
 
 /* appearance */
-static unsigned int borderpx  = 1;        /* border pixel of windows */
+static unsigned int borderpx  = 0;        /* border pixel of windows */
 static unsigned int snap      = 32;       /* snap pixel */
 static unsigned int gappih    = 0;       /* horiz inner gap between windows */
 static unsigned int gappiv    = 0;       /* vert inner gap between windows */
